17ca802dbf21a20799dddff5623fabd6b8f443a5
Artyom Sokolov <unnutz@gmail.com>
2017-01-25 19:26:01 +0200

    Yes?

--- TestProject/Global.asax.cs
+++ TestProject/Global.asax.cs
@@ -16,12 +16,11 @@ namespace TestProject
 
 		protected void Application_BeginRequest()
 		{
-			Response.Headers.Add("Access-Control-Allow-Origin", "*");
-			Response.Headers.Add("Access-Control-Allow-Methods", "*");
-			Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
-
 			if (Request.HttpMethod == "OPTIONS")
 			{
+				Response.Headers.Add("Access-Control-Allow-Origin", "*");
+				Response.Headers.Add("Access-Control-Allow-Methods", "*");
+				Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
 				Response.Flush();
 			}
 		}
