6b21d3101843d1fa0fc170984dc0a036b869ffa7
Artyom Sokolov <unnutz@gmail.com>
2017-01-25 19:23:30 +0200

    Finally?

--- TestProject/Global.asax.cs
+++ TestProject/Global.asax.cs
@@ -13,5 +13,17 @@ namespace TestProject
 			GlobalConfiguration.Configure(WebApiConfig.Register);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 		}
+
+		protected void Application_BeginRequest()
+		{
+			Response.Headers.Add("Access-Control-Allow-Origin", "*");
+			Response.Headers.Add("Access-Control-Allow-Methods", "*");
+			Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
+
+			if (Request.HttpMethod == "OPTIONS")
+			{
+				Response.Flush();
+			}
+		}
 	}
 }
